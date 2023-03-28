c848c1a77e1135921d11db9d1295d33aec1d560e
David <davidbrandon713@gmail.com>
2023-03-28 03:46:05 -0500

    messing with api calls

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Express Server for WFM-Frontend
+# Express Server for [WFM-Frontend](https://github.com/davidbrandon713/wfm-frontend)
 - clone the repository
 - run `npm install`
 - run `npm start` (or launch the .bat file)
--- server.js
+++ server.js
@@ -7,48 +7,48 @@ app.use(express.json())
 app.use(cors())
 
 app.get('/:item', async (req, res) => {
-  try {
-    await fetch(`https://api.warframe.market/v1/items/${req.params.item}`, {
-      method: 'GET',
-      headers: {
-        'Access-Control-Allow-Origin': '*',
-      },
-    })
-      .then((res) => res.json())
-      .then((data) =>
-        data.payload.item.items_in_set.filter((item) => {
-          return item.url_name === req.params.item
-        })
-      )
-      .then((data) => res.status(200).json(data[0].en.item_name))
-  } catch (err) {
-    res.status(500).json({ message: err.message })
-  }
+	try {
+		await fetch(`https://api.warframe.market/v1/items/${req.params.item}`, {
+			method: 'GET',
+			headers: {
+				'Access-Control-Allow-Origin': '*',
+			},
+		})
+			.then((res) => res.json())
+			.then((data) =>
+				data.payload.item.items_in_set.filter((item) => {
+					return item.url_name === req.params.item
+				})
+			)
+			.then((data) => res.status(200).json(data[0]))
+	} catch (err) {
+		res.status(500).json({ message: err.message })
+	}
 })
 
 app.get('/:item/orders', async (req, res) => {
-  try {
-    await fetch(
-      `https://api.warframe.market/v1/items/${req.params.item}/orders`,
-      {
-        method: 'GET',
-        headers: {
-          'Access-Control-Allow-Origin': '*',
-        },
-      }
-    )
-      .then((res) => res.json())
-      .then((data) =>
-        data.payload.orders.filter((order) => {
-          return order.user.status === 'ingame'
-        })
-      )
-      .then((data) => res.status(200).json(data))
-  } catch (err) {
-    res.status(500).json({ message: err.message })
-  }
+	try {
+		await fetch(
+			`https://api.warframe.market/v1/items/${req.params.item}/orders`,
+			{
+				method: 'GET',
+				headers: {
+					'Access-Control-Allow-Origin': '*',
+				},
+			}
+		)
+			.then((res) => res.json())
+			.then((data) =>
+				data.payload.orders.filter((order) => {
+					return order.user.status === 'ingame'
+				})
+			)
+			.then((data) => res.status(200).json(data))
+	} catch (err) {
+		res.status(500).json({ message: err.message })
+	}
 })
 
 app.listen(port, () => {
-  console.log(`\x1b[33mServer running on port ${port}.\x1b[39m`)
+	console.log(`\x1b[33mServer running on port ${port}.\x1b[39m`)
 })
