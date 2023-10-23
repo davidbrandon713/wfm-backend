b00703c89f75c18ea8891a885a517e4a235a52b3
David <davidbrandon713@gmail.com>
2023-10-23 14:28:33 -0500

    update server

--- server.js
+++ server.js
@@ -2,34 +2,29 @@ const express = require('express')
 const cors = require('cors')
 
 const app = express()
-const port = 3002
+const port = 8081
 app.use(express.json())
 app.use(cors())
 
-app.get('/:item', async (req, res) => {
-	try {
-		await fetch(`https://api.warframe.market/v1/items/${req.params.item}`, {
-			method: 'GET',
-			headers: {
-				'Access-Control-Allow-Origin': '*',
-			},
-		})
-			.then((res) => res.json())
-			.then((data) =>
-				data.payload.item.items_in_set.filter((item) => {
-					return item.url_name === req.params.item
-				})
-			)
-			.then((data) => res.status(200).json(data[0]))
-	} catch (err) {
-		res.status(500).json({ message: err.message })
-	}
-})
+// app.get('/:item', async (req, res) => {
+// 	try {
+// 		await fetch(`https://api.warframe.market/v1/items/${req.params.item}`, {
+// 			method: 'GET',
+// 			headers: {
+// 				'Access-Control-Allow-Origin': '*',
+// 			},
+// 		})
+// 			.then((response) => response.json())
+// 			.then((data) => res.status(200).json(data[0]))
+// 	} catch (err) {
+// 		res.status(500).json({ message: err.message })
+// 	}
+// })
 
 app.get('/:item/orders', async (req, res) => {
 	try {
 		await fetch(
-			`https://api.warframe.market/v1/items/${req.params.item}/orders`,
+			`https://api.warframe.market/v1/items/${req.params.item}/orders?include=item`,
 			{
 				method: 'GET',
 				headers: {
@@ -37,13 +32,9 @@ app.get('/:item/orders', async (req, res) => {
 				},
 			}
 		)
-			.then((res) => res.json())
-			.then((data) =>
-				data.payload.orders.filter((order) => {
-					return order.user.status === 'ingame'
-				})
-			)
+			.then((response) => response.json())
 			.then((data) => res.status(200).json(data))
+      .then(console.log(`GET ${req.params.item} orders`))
 	} catch (err) {
 		res.status(500).json({ message: err.message })
 	}
